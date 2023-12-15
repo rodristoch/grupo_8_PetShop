f422ef7d3bb9618aad6b9da3cb4d93931e537d65
Agustin Macko <agustinmacko@MacBook-Pro-de-Agustin.local>
2023-12-15 10:04:24 -0300

    organizado y comentado app + creado v1 de alta con sus estilos

--- src/app.js
+++ src/app.js
@@ -3,18 +3,26 @@ const { get } = require("http");
 
 const path = require("path");                                                        //para requerir path (para hacer un ruteo mas sencillo para express)
 
-
+// ************ express()************
 const app = express();                                                               //ejecuto express
 
+// ************ Middlewares ************
+
+// ************ Template Engine  ************
+app.set('view engine', 'ejs'); // configuramos ejs como motor de plantilla 
+app.set('views', './src/views') // indicamos a express que los archivos ejs por defecto los debe buscar en la carpeta src-views 
+
+
+app.use(express.static(path.resolve(__dirname, "../public")));     //para hacer uso de la carpeta public
+
+// ************ Route System  ************
 const mainRoutes = require('./routes/mainRoutes.js')
-
 const userRoutes = require('./routes/userRoutes.js')
-
 const productRoutes = require('./routes/productRoutes.js')
 
-
-app.listen (3100, () => console.log("Servidor corriendo en puerto 3100"));           //levanto el servidor y lo pongo a escuchar
-
+app.use("/", mainRoutes); 
+app.use("/users", userRoutes); 
+app.use("/productos", productRoutes);
 
 /* app.get("/home", (req, res) => {
     
@@ -22,16 +30,5 @@ app.listen (3100, () => console.log("Servidor corriendo en puerto 3100"));      
 
 }); */
 
-app.use("/", mainRoutes); 
+app.listen (3100, () => console.log("Servidor corriendo en puerto 3100"));           //levanto el servidor y lo pongo a escuchar
 
-app.use("/users", userRoutes); 
-
-app.use("/productos", productRoutes);
-
-
-app.use(express.static(path.resolve(__dirname, "../public")));     //para hacer uso de la carpeta public
-
-
-app.set('view engine', 'ejs'); // configuramos ejs como motor de plantilla 
-
-app.set('views', './src/views') // indicamos a express que los archivos ejs por defecto los debe buscar en la carpeta src-views 
