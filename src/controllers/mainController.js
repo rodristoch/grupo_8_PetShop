c4b36257253793e1a53d9e3eac240a3bb99f8715
rodristoch <stoke870@gmail.com>
2023-12-31 12:38:00 -0300

    reorganizo los json y el mainController

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,44 +1,34 @@
 const fs = require("fs");
 const path = require("path");
 
-const accesoriosFilePath = path.join(__dirname, '../data/productos-accesorios.json');
-/* const accesorios = JSON.parse(fs.readFileSync(accesoriosFilePath, 'utf-8')); */
+const productosPerroFilePath = path.join(__dirname, '../data/productos-perro.json');
+/* const productosPerro = JSON.parse(fs.readFileSync(productosPerroFilePath, 'utf-8')); */
 
-const comidaFilePath = path.join(__dirname, '../data/productos-comida.json');
-/* const comida = JSON.parse(fs.readFileSync(comidaFilePath, 'utf-8')); */
+const productosGatoFilePath = path.join(__dirname, '../data/productos-gato.json');
+/* const productosGato = JSON.parse(fs.readFileSync(productosGatoFilePath, 'utf-8')); */
 
-const higieneFilePath = path.join(__dirname, '../data/productos-higiene.json');
-/* const higiene = JSON.parse(fs.readFileSync(higieneFilePath, 'utf-8')); */
 
-const juguetesFilePath = path.join(__dirname, '../data/productos-juguetes.json');
-/* const juguetes = JSON.parse(fs.readFileSync(juguetesFilePath, 'utf-8')); */
 
 const mainController = {
     index : (req, res) => {
-        const accesorios = JSON.parse(fs.readFileSync(accesoriosFilePath, 'utf-8'));
-        const comida = JSON.parse(fs.readFileSync(comidaFilePath, 'utf-8'));
-        const higiene = JSON.parse(fs.readFileSync(higieneFilePath, 'utf-8'));
-        const juguetes = JSON.parse(fs.readFileSync(juguetesFilePath, 'utf-8'));
 
         //productos perros
-        const accesoriosPerro = accesorios.filter(product => {return product.category == "Perro"})
-        const comidaPerro = comida.filter(product => {return product.category == "Perro"})
-        const higienePerro = higiene.filter(product => {return product.category == "Perro"})
-        const juguetesPerro = juguetes.filter(product => {return product.category == "Perro"})
+        const productosPerro = JSON.parse(fs.readFileSync(productosPerroFilePath, 'utf-8'));
 
         //productos gatos
-        const accesoriosGato = accesorios.filter(product => {return product.category == "Gato"})
-		const comidaGato = comida.filter(product => {return product.category == "Gato"})
-        const higieneGato = higiene.filter(product => {return product.category == "Gato"})
-        const juguetesGato = juguetes.filter(product => {return product.category == "Gato"})
+        const productosGato = JSON.parse(fs.readFileSync(productosGatoFilePath, 'utf-8'));
+
+        //productos perros accesorios
+        const productosPerroAccesorios = productosPerro.filter(product => {return product.category == "Accesorio"});
+
+        //productos gatos accesorios
+        const productosGatoAccesorios = productosGato.filter(product => {return product.category == "Accesorio"});
 
         //productos con descuentos
-        const accesoriosDescuento = accesorios.filter(product => {return product.discount == "Si"})
-		const comidaDescuento = comida.filter(product => {return product.discount == "Si"})
-        const higieneDescuento = higiene.filter(product => {return product.discount == "Si"})
-        const juguetesDescuento = juguetes.filter(product => {return product.discount == "Si"})
+        const productosPerroConDescuento = productosPerro.filter(product => {return product.discount == "Si"});
+		const productosGatoConDescuento = productosGato.filter(product => {return product.discount == "Si"});
 
-		res.render("index.ejs", {accesoriosPerro, comidaPerro, higienePerro, juguetesPerro, accesoriosGato, comidaGato, higieneGato, juguetesGato, accesoriosDescuento, comidaDescuento, higieneDescuento, juguetesDescuento});
+		res.render("index.ejs", {productosPerro, productosGato, productosPerroAccesorios, productosGatoAccesorios, productosPerroConDescuento, productosGatoConDescuento});
 	},
 
 
