201d6f4c77a6bd57e8282713fc94c7048483d867
rodristoch <stoke870@gmail.com>
2024-01-14 16:35:15 -0300

    en el index pongo productos sin descuentos en las cards de abajo

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -23,7 +23,11 @@ const mainController = {
         //productos gatos accesorios
         const productosGatoAccesorios = productosGato.filter(product => {return product.category == "Accesorio"});
 
-        
+        //accesorios sin descuentos de perros
+        const accesoriosPerroSinDescuento = productosPerroAccesorios.filter(product => {return product.discount == "No"});
+        //accesorios sin descuentos de gatos
+		const accesoriosGatoSinDescuento = productosGatoAccesorios.filter(product => {return product.discount == "No"});
+
         //productos con descuentos de perros
         const productosPerroConDescuento = productosPerro.filter(product => {return product.discount == "Si"});
         //productos con descuentos de gatos
@@ -32,7 +36,7 @@ const mainController = {
         //usuario que se loguea
         const usuario = req.session.userLogueado
 
-		res.render("index.ejs", {productosPerro, productosGato, productosPerroAccesorios, productosGatoAccesorios, productosPerroConDescuento, productosGatoConDescuento, usuario});
+		res.render("index.ejs", {productosPerro, productosGato, productosPerroAccesorios, productosGatoAccesorios, productosPerroConDescuento, productosGatoConDescuento, accesoriosPerroSinDescuento, accesoriosGatoSinDescuento, usuario});
         
 	}
 }
