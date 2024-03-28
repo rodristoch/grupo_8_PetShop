17004e932413592ab930671205e05fe4df450356
NicoV1997 <nicolasvillamil1997@gmail.com>
2024-03-28 15:35:40 -0300

    Terminamos las validaciones de front para el login

--- public/js/validacionlogin.js
+++ public/js/validacionlogin.js
@@ -1,51 +1,7 @@
-/* window.addEventListener("load", () => {
-
-    //capturo elementos
-
-    let form = document.querySelector('#formulario');
-    let email = document.querySelector('#email');
-    let password = document.querySelector('#password');
-    let btnSubmit = document.querySelector('#btnSubmit');
-
-
-    // capturo errores 
-
-    let errorEmail = document.querySelector('.errorEmail');
-    let errorPassword = document.querySelector('.errorPassword');
-
-
-    btnSubmit.addEventListener('click', (event) => {
-
-        event.preventDefault();
-
-        let errores = {};
-
-        let reg = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-
-        if (email.value.length < 1) {
-            errores.email = 'Este campo debe estar completo'
-        } else if (!reg.test(email.value)) {
-            errores.email = 'El email introducido no es válido'
-        }
-        if (password.value.length < 1) {
-            errores.password = 'Este campo debe estar completo'
-        } else if (password.value.length < 8) {
-            errores.password = 'La cantraseña debe tener al menos 8 caracteres'
-        }
-        if (Object.keys(errores).length >= 1) {
-            
-            errorEmail.innerText = (errores.email) ? errores.email : '';
-            errorPassword.innerText = (errores.password) ? errores.password : '';
-        } else {
-            form.submit();
-        }
-    });
-}); */
-
 window.addEventListener("load", () => {
 
     //capturo elementos
-    let form = document.querySelector('#formulario');
+    let form = document.querySelector('.formulario');
     let email = document.querySelector('#email');
     let password = document.querySelector('#password');
     let errorEmail = document.querySelector('.errorEmail');
@@ -71,6 +27,10 @@ window.addEventListener("load", () => {
 
         errorEmail.innerText = errores.email || '';
         errorPassword.innerText = errores.password || '';
+
+        errores.email ? email.classList.add("input_error-front") : email.classList.add("input_good-front")  
+        errores.password ? password.classList.add("input_error-front") : password.classList.add("input_good-front")  
+            
 
         if (Object.keys(errores).length === 0) {
             form.submit();
