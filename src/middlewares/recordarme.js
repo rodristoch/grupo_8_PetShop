2b6edf8ac2ee75656a601b5a3f3f342457d380f7
rodristoch <stoke870@gmail.com>
2024-04-05 17:28:09 -0300

    recordarme middleware con la database (no estaba y no funcionaba la cookie de recordar usuario porq estaba con los json)

--- src/middlewares/recordarme.js
+++ src/middlewares/recordarme.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const path = require("path")
+const db = require('../database/models');
+const sequelize = db.sequelize;
+const Op = db.Sequelize.Op;
 
 const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
 // const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
@@ -8,24 +11,31 @@ function recordarme(req, res, next){
 
     if(req.cookies.recordarme != undefined && req.session.userLogueado == undefined){ //si esta cookie existe (el nombre de la cookie debe ser el de la cookie creada en el controller) y a su vez, no está el user en sesion...
         //traigo los usuarios
-        const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+        /* const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8')); */
 
-        let userALoguearse
+        let users = db.Usuario.findAll({
+            include: ["permisos"],
+        })
 
-        for(let i=0; i < users.length; i++){  //recorremos todos los usuarios
+        .then(users => {
 
-            if(users[i].email == req.cookies.recordarme){  //preguntamos si el email de ese usuario (el de la posicion i) es igual al email que está en la cookie
+            let userALoguearse
 
-                userALoguearse = users[i]  //el usuario q se loguea es el q matchea en la posicion i (lo encontró)
+            for(let i=0; i < users.length; i++){  //recorremos todos los usuarios
 
-                break;
-                
-            }     
-        }
+                if(users[i].email == req.cookies.recordarme){  //preguntamos si el email de ese usuario (el de la posicion i) es igual al email que está en la cookie
 
-        req.session.userLogueado = userALoguearse;  //si encontró al usuario lo guardo en session
+                    userALoguearse = users[i]  //el usuario q se loguea es el q matchea en la posicion i (lo encontró)
 
+                    break;
+                    
+                }     
+            }
+
+            req.session.userLogueado = userALoguearse;  //si encontró al usuario lo guardo en session
         
+        })
+
     } 
 
     next();
